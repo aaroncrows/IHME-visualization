242b1abd80e18f5c9f68e16ee056d5ea1e89544c
Aaron Krause <aaronjkrause@gmail.com>
2015-09-03 21:02:17 -0700

    select menu working, checkbox kind of working

--- public/main.js
+++ public/main.js
@@ -1,11 +1,10 @@
 
 function sortByLocationNameThenAgeGroup(dataset, ageGroup, metric) {
   var sorted = {};
-  var yearsSeen = {
-    male: {},
-    female: {}
-  };
+  var countriesSeen = {};
+
   ageGroup = ageGroup || 'age_group'
+  sorted.countryNames = [];
 
   for (var i = 0; i < dataset.length; i++) {
     var curr = dataset[i];
@@ -20,6 +19,10 @@ function sortByLocationNameThenAgeGroup(dataset, ageGroup, metric) {
     //if (!sorted[currLocation][currAgeGroup]) sorted[currLocation][currAgeGroup] = [];
     if(currAgeGroup === ageGroup && curr.sex != 'both' && curr.metric == metric) {
       sorted[currLocation][curr.sex].push(curr);
+      if(!countriesSeen[currLocation]) {
+        sorted.countryNames.push(currLocation);
+        countriesSeen[currLocation] = true;
+      }
     }
   };
   return sorted;
@@ -30,6 +33,8 @@ var width = 500;
 
 
 var COUNTRY = "United States";
+var MALE = true;
+var FEMALE = true;
 var YEAR_STANDARD = 1989;
 
 var drawLine = d3.svg.line()
@@ -39,11 +44,27 @@ var drawLine = d3.svg.line()
 
 var chart = d3.select('#display')
 
+var menu = d3.select('#countries');
+var maleCheck = d3.select('#male-check');
+var femaleCheck = d3.select('#female-check');
+
 d3.csv('./data.csv', function(data) {
 
   data = sortByLocationNameThenAgeGroup(data, "20+ yrs, age-standardized", 'obese');
   console.log(data);
-  // chart.selectAll('path').enter().data(data);
+  console.log(menu.selectAll('option'))
+
+
+  menu.selectAll('option')
+    .data(data.countryNames)
+    .enter()
+    .append('option')
+    .attr('value', function(d) { return d })
+    .text(function(d) {return d})
+
+    console.log(d3.select('#menu').node())
+
+
   var female = chart.append('path')
     .attr('class', 'female')
     .attr('d', drawLine(data[COUNTRY].female))
@@ -61,32 +82,38 @@ d3.csv('./data.csv', function(data) {
 
 
   function update(data, COUNTRY) {
+    if(MALE) {
     male.transition()
       .attr('d', drawLine(data[COUNTRY].male))
       .duration(750)
-      .ease('easeOutQuint')
+      .ease('easeOutQuint');
+    } else {
+      male.remove();
+    }
+    if(FEMALE) {
+      female.transition()
+        .attr('d', drawLine(data[COUNTRY].female))
+        .duration(750)
+        .ease('easeOutQuint');
+    } else {
+      female.remove();
+    }
 
-    female.transition()
-      .attr('d', drawLine(data[COUNTRY].female))
-      .duration(750)
-      .ease('easeOutQuint')
-
-  //   chart.selectAll('.dot')
-  //   .data(data[COUNTRY].male)
-  //   .enter().append('path')
-  //   .attr('class', function(d) {
-  //     return d.sex
-  //   })
-  //   .attr('d', drawLine(data[COUNTRY].male))
-  //   .attr('stroke-width', 2)
-  //   .attr('stroke', 'blue')
-  //   .attr('fill', 'none')
   }
+  //event handlers
+  menu.on('change', function() {
+    COUNTRY = this.value;
+    update(data, COUNTRY);
+  });
 
+  maleCheck.on('change', function() {
+    MALE = !MALE;
+    update(data, COUNTRY);
+  });
 
-  setTimeout(function() {
-    update(data, 'Afghanistan')
-  }, 2000)
-
+  femaleCheck.on('change', function() {
+    FEMALE = !FEMALE;
+    update(data, COUNTRY);
+  });
   
-})
+});
